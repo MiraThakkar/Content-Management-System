d1ab3214b915bd49cd81643adb7bc8c4455c382b
Mira Thakkar <mthakkar84@gmail.com>
2020-02-22 09:26:42 -0500

    updated employeebydept function

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 require('dotenv').config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
 const cTable = require('console.table');
-
-
-
-
 
 var connection = mysql.createConnection({
         host: process.env.host,
@@ -15,33 +10,23 @@ var connection = mysql.createConnection({
         database: process.env.database
 });
 
-
-
-
 // connect to the mysql server and sql database
 connection.connect(function(err) {
     if (err) throw err;
 
-    console.log(process.env.username);
-    console.log(process.env.host);
-    console.log(process.env.password);
-    console.log(process.env.database);
-
-
+    
     // run the start function after the connection is made to prompt the user
     startApp();
 });
 
-
 // function which prompts the user for what action they should take
-
 function startApp() {
     inquirer
       .prompt({
         name: "action",
         type: "list",
         message: "What would you like to do?",
-        choices: ["View All Employees", "View Employees By Department", "View Employees By Role", "Add New Employee",
+        choices: ["View All Employees", "View All Departments", "View Employees By Department", "View Employee Roles", "View Employees By Role", "Add New Employee",
          "Edit Employee Manager", "Edit Employee Role", "Remove Employee"]
       })
       .then(function(answer) {
@@ -54,32 +39,43 @@ function startApp() {
             viewDepartments();
             break;
     
-          case "view Employee Roles":
+          case "View Employees By Department":
+            employeeByDepartment();
+            break;
+  
+          case "View Employee Roles":
             viewRoles();
             break;
     
-          case "View Employees by Role":
-            songSearch();
+          case "View Employees By Role":
+            console.log("view Employees By Role - Function to be written");
             break;
 
           case "Add New Employee":
             addNewEmployee();
             break;
     
-          case "View Employees by Department":
-            connection.end();
+          case "Edit Employee Manager":
+            console.log("Edit Employee Manager - Function to be written");
             break;
+          
+          case "Edit Employee Role":
+            console.log("Edit Employees Role - Function to be written");
+            break;
+          
+          case "Remove Employee":
+            console.log("Remove Employee - Function to be written");
+            break;
+                      
+          default:
+            connection.end()
+          
         }
       });
   }
         
-
-
-
 // ========================View all employees===================================
-
 function viewEmployees() {
-
     connection.query("SELECT * FROM employee", function(err, results) {
         if (err) throw err;
         console.table(results);
@@ -87,13 +83,10 @@ function viewEmployees() {
         return results;
 
     });
-
 }
 
 // ========================View all departments===================================
-
 function viewDepartments() {
-
   connection.query("SELECT * FROM department", function(err, results) {
       if (err) throw err;
       console.table(results);
@@ -101,27 +94,20 @@ function viewDepartments() {
       return results;
 
   });
-
 }
 
 // ========================View all Roles===================================
-
 function viewRoles() {
-
   connection.query("SELECT * FROM role", function(err, results) {
       if (err) throw err;
       console.table(results);
       connection.end();
       return results;
-
   });
-
 }
 
 //==========================Remove Employee========================================
-
 function removeEmployee() {
-    console.log("Inside remove employee function");
     inquirer
     .prompt({
       name: "deleteemployee",
@@ -131,46 +117,46 @@ function removeEmployee() {
        "Edit Employee Manager", "Edit Employee Role", "Remove Employee"]
     })
     .then(function(answer) {
-
       console.log(answer);
-
     });
-
 }
-
 
 // ===============================View employee by department===============================
 function employeeByDepartment() {
-
-  console.log("Inside  employeeByDept function");
-
-  return inquirer
-  .prompt({
-    name: "name",
-    type: "list",
-    message: "Select Department",
-    choices: ["Finance", "Information Technology", "Accounting", "Marketing", "Production"]
-  })
-    .then(function(answer){
-      var query = "SELECT e.first_name, e.last_name, d.name FROM employee e, role r, department d WHERE e.role_id = r.id AND r.department_id = d.id AND  ?";
-      connection.query(query, { name: answer.name}, function(err, results) {
-        if (err) throw err;
-        console.table(results);
-        connection.end();
-        return results;
-
-      });
+  connection.query("SELECT name FROM department", function(err, results) {
+    if (err) throw err;        
+    inquirer
+      .prompt({
+        name: "name",
+        type: "list",
+        message: "Select Department",
+        choices: function() {
+          //start of function
+          var departmentList = [];
+          for (var i = 0; i < results.length; i++){
+            departmentList.push(results[i].name);
+          }    
+          return departmentList;
+        }
+      })
+      .then(function(answer){
+        var query = "SELECT e.first_name, e.last_name, d.name FROM employee e, role r, department d WHERE e.role_id = r.id AND r.department_id = d.id AND  ?";
+        connection.query(query, { name: answer.name}, function(err, results) {
+          if (err) throw err;
+          console.table(results);
+          connection.end();
+          return results;
+        });
       // console.log(name);
     });
-
+  }); //end of first connection.query - select name from department
 }
 
 //================================View employee by role=============================================
 
 function employeeByRole() {
 
-  console.log("Inside  employeeByRole function");
-
+  
   return inquirer
   .prompt({
     name: "empName",
